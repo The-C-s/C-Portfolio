0de006390cdceddbb4e99517c910851c7866082a
Newaz Saif Syed Ahammad <snewaz@Newazs-Air.modem>
2020-08-30 20:39:42 +1000

    added dependencies, imported libraries

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {Provider} from 'react-redux';
+
 import './App.css';
 
 import Navbar from "./components/Navbar";
  
 function App() {
  return (
-   <div className="App">
-     <Navbar/>
-   </div>
+   <Provider>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Route/>
+        <Switch>
+        </Switch>   
+      </div>
+    </Router>
+   </Provider>
  );
 }
  
