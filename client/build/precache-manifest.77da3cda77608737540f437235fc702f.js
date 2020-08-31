748e79bdb0a31406b8e16dab04e75e36ec3d916a
vyxnn <69046734+vyxnn@users.noreply.github.com>
2020-08-31 18:00:27 +1000

    added build script

---
+++ client/build/precache-manifest.77da3cda77608737540f437235fc702f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4b505653c270d1f80966321eccade0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f114664786685b4e751",
+    "url": "/static/css/main.30d922f5.chunk.css"
+  },
+  {
+    "revision": "c513dce69581c118aa86",
+    "url": "/static/js/2.559ff8fd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.559ff8fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f114664786685b4e751",
+    "url": "/static/js/main.f3ca3b19.chunk.js"
+  },
+  {
+    "revision": "4fb9307be86c7ffbd3ff",
+    "url": "/static/js/runtime-main.91d3deb0.js"
+  }
+]);
