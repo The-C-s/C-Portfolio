b32212227bf0adf2359a6ea4c76a2b7406fd88ea
Brodie Daff <bdaff@student.unimelb.edu.au>
2020-09-10 18:02:46 +1000

    Create empty Dashboard

---
+++ client/src/components/dashboard/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Feed extends Component {
+  render() {
+    return(
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Your Posts</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+        </div>
+      </div>
+    );
+  }
+}
--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import Navbar from '../layout/Navbar';
+import SideNavBar from '../layout/SideNavbar';
+import Feed from '../dashboard/Feed';
+
 const Dashboard = () => {
   return(
-    <div>
-      
-    </div>
+    <React.Fragment>
+      <Navbar/>
+      <div class="container-fluid">
+        <div class="row">
+          <SideNavBar/>
+          <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+            <Feed/>
+          </main>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
