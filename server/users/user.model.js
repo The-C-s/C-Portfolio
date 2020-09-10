fa7d7633ff8329d0e27eb7b3dd2f93a08b4054e5
Vikki <69046734+vyxnn@users.noreply.github.com>
2020-09-10 20:27:30 +1000

    Fixing merges

--- server/users/user.model.js
+++ server/users/user.model.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
-    email: { type: String, unique: true, required: true },
-    hash: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-
  //Schema for user information
 const schema = new Schema({
     email: { type: String, unique: true, required: true },
