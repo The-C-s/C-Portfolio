b055bef43cf4d8bff5f9eab301c41b1f124d5d40
Brodie Daff <bdaff@student.unimelb.edu.au>
2020-09-13 17:14:55 +1000

    Add axios.defaults for auth header management

--- client/src/common/api.js
+++ client/src/common/api.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
 
-//const BASE = 'http://cportfolio.herokuapp.com';
-const BASE = 'http://localhost:39921';
-const AUTH_USER = `${BASE}/users/authenticate`;
-const GET_ALL_CONTENT = `${BASE}/content/`;
-const CREATE_CONTENT = `${BASE}/content/create`;
-const EDIT_CONTENT = `${BASE}/content`;
-const DELETE_CONTENT = `${BASE}/content`;
+// Alter based on environment
+axios.defaults.baseURL = 'https://cportfolio.herokuapp.com';
+
+const AUTH_USER = '/users/authenticate';
+const CONTENT = '/content/';
+const CREATE_CONTENT = '/content/create';
+
+const setAuthHeader = (authType, token) => axios.defaults.headers.common['Authorization'] = `${authType} ${token}`;
 
 const authenticateUser = async user => await axios.post(AUTH_USER, user);
 
-const getAllContent = async (authType, token) => await axios.get(GET_ALL_CONTENT, { headers: { 'Authorization': `${authType} ${token}` } });
+const getAllContent = async (authType, token) => await axios.get(CONTENT, { headers: { 'Authorization': `${authType} ${token}` } });
 
 const createContent = async (content, authType, token) => await axios.post(CREATE_CONTENT, content, { headers: { 'Authorization': `${authType} ${token}` } });
 
-const editContent = async (id, content, authType, token) => await axios.put(`${EDIT_CONTENT}/${id}`, content, { headers: { 'Authorization': `${authType} ${token}` } });
+const editContent = async (id, content, authType, token) => await axios.put(`${CONTENT}${id}`, content, { headers: { 'Authorization': `${authType} ${token}` } });
 
-const deleteContent = async (id, authType, token) => await axios.delete(`${DELETE_CONTENT}/${id}`, { headers: { 'Authorization': `${authType} ${token}` } });
+const deleteContent = async (id, authType, token) => await axios.delete(`${CONTENT}${id}`, { headers: { 'Authorization': `${authType} ${token}` } });
 
-export default { authenticateUser, getAllContent, createContent, editContent, deleteContent };
+export default { setAuthHeader, authenticateUser, getAllContent, createContent, editContent, deleteContent };
--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,15 +1,16 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import API from '../../common/api';
+import api from '../../common/api';
 
 export const login = createAsyncThunk(
   'user/authenticate',
-  user => API.authenticateUser(user)
+  credentials => api.authenticateUser(credentials)
 );
 
 const user = createSlice({
   name: 'user',
   initialState: {
-    isAuthenticated: false
+    isAuthenticated: false,
+    authType: 'Bearer'
   },
   reducers: {
     logout: (state, action) => {
@@ -21,13 +22,16 @@ const user = createSlice({
   extraReducers: {
     [login.fulfilled]: (state, action) => {
 
+      // Persist JWT to local storage
       localStorage.setItem('token', action.payload.data.token);
+
+      // Tell axios to send auth token with every future request
+      api.setAuthHeader(state.authType, action.payload.data.token);
 
       return {
         ...state,
         ...action.payload.data,
-        isAuthenticated: true,
-        authType: 'Bearer'
+        isAuthenticated: true
       }
     }
   }
