632a2baea7489211d4ab68e058a130b7681b5aee
vyxnn <69046734+vyxnn@users.noreply.github.com>
2020-10-01 17:41:16 +1000

    able to edit the arrays in profiles

--- client/src/features/profile/EditProfile.js
+++ client/src/features/profile/EditProfile.js
@@ -1,80 +1,189 @@
-//Can probably split this into different fields 
-import React, { useState } from 'react';
-import { useDispatch} from 'react-redux';
-import { editProfile, getProfile } from './profileSlice';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row'; 
+//Can probably split this into different fields
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { editProfile, getProfile } from "./profileSlice";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+//import Row from 'react-bootstrap/Row';
+import InputGroup from "react-bootstrap/InputGroup";
 
-//Problem getting a profile always takes 2 seconds,
-//So the initial values are always empty or fails if I don't set them 
-//However EditProfile seems to be constructed only once, with said empty profile 
-//How to refresh once it has an actual profile? 
-
-//Still messed up if u click on edit before it loads 
-//Maybe only display edit button once it's loaded? 
 export default function EditProfile({ profile, show, closeHandler }) {
   //_profile is the state variable, updateProfile is a function that updates the state
   //Initial state of profile always takes some time to update
-
   const [_profile, updateProfile] = useState(profile);
-  const [_education, updateEducation] = useState(profile.education); 
-  const [_experience, updateExperience] = useState(profile.experience); 
-  const [_projects, updateProjects] = useState(profile.projects); 
+  const [_education, updateEducation] = useState(profile.education);
+  const [_experience, updateExperience] = useState(profile.experience);
+  const [_projects, updateProjects] = useState(profile.projects);
   const dispatch = useDispatch();
+  console.log(_projects);
+  console.log(profile);
+
+  //Saves all changes
   const editClickHandler = () => {
     dispatch(editProfile(_profile))
       .then(() => dispatch(getProfile(_profile.id)))
       .then(() => closeHandler());
-  }
+  };
 
-  
-  const onChangeHandler = e => updateProfile({ ..._profile, [e.target.id]: e.target.value });
-  //Both of these functions work seperately, but updating any field after adding a new one always resets the array to empty 
-  //Assume its an issue with concurrent modification? 
-  //Updates a single field 
-  //VVV this function needs to update the specific field not replace it with the input, e.target.id is probably not correct here  
-  const onChangeEducationHandler = e => {
-      const tmp = [..._education]; 
-      tmp[e.target.id] = e.target.value; 
-      updateEducation(tmp)}; 
-  //Adds an empty education field 
-  const addEducationField = e => updateEducation([..._education, '']); 
-  
-  return(
+  //Updates and sets education field in profile
+  const onChangeEducationHandler = (e) => {
+    //Copy and updates tmp array
+    const tmp = [..._education];
+    tmp[e.target.id] = e.target.value;
+    //Updates education array and profile in react stae
+    updateEducation(tmp);
+    updateProfile({ ..._profile, education: tmp });
+  };
+
+  //Adds an empty education field
+  const addEducationField = () => {
+    const tmp = [..._education, ""];
+    updateEducation(tmp);
+    updateProfile({ ..._profile, education: tmp });
+  };
+  //Deletes an education field
+  const deleteEducationField = (id) => {
+    const tmp = [..._education];
+    //Removes index
+    tmp.splice(id, 1);
+    updateEducation(tmp);
+    updateProfile({ ..._profile, education: tmp });
+  };
+
+  const onChangeExperienceHandler = (e) => {
+    const tmp = [..._experience];
+    tmp[e.target.id] = e.target.value;
+    updateExperience(tmp);
+    updateProfile({ ..._profile, experience: tmp });
+  };
+
+  const addExperienceField = () => {
+    const tmp = [..._experience, ""];
+    updateExperience(tmp);
+    updateProfile({ ..._profile, experience: tmp });
+  };
+
+  const deleteExperienceField = (id) => {
+    const tmp = [..._experience];
+    tmp.splice(id, 1);
+    updateExperience(tmp);
+    updateProfile({ ..._profile, experience: tmp });
+  };
+
+  const onChangeProjectHandler = (e) => {
+    const tmp = [..._projects];
+    tmp[e.target.id] = e.target.value;
+    updateProjects(tmp);
+    updateProfile({ ..._projects, projects: tmp });
+  };
+
+  const addProjectField = () => {
+    const tmp = [..._projects, ""];
+    console.log(tmp);
+    updateProjects(tmp);
+    updateProfile({ ..._profile, projects: tmp });
+  };
+
+  const deleteProjectField = (id) => {
+    const tmp = [..._projects];
+    tmp.splice(id, 1);
+    updateProjects(tmp);
+    updateProfile({ ..._profile, projects: tmp });
+  };
+
+  return (
     <Modal size="lg" show={show} onHide={closeHandler}>
-      <Modal.Header><Modal.Title>Edit Profile</Modal.Title></Modal.Header>
+      <Modal.Header>
+        <Modal.Title>Edit Profile</Modal.Title>
+      </Modal.Header>
       <Modal.Body>
         <Form>
           <Form.Group controlId="education">
             <Form.Label>Education</Form.Label>
-            {_education.map(education =>
-                    <Row>
-                       <Form.Control as = "textarea" rows="1" value = {education} onChange={onChangeEducationHandler}/> 
-                    </Row>)}
-            <Button onClick={addEducationField}>Add Education</Button>
           </Form.Group>
-          <Form.Group controlId="experience">
-            <Form.Label>Experience</Form.Label>
-                {_experience.map(experience =>
-                    <Row>
-                       <Form.Control as = "textarea" rows="1" value = {experience}/> 
-                    </Row>)}
+          {_education.map((education, i) => (
+            <Form.Group controlId={i}>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  as="textarea"
+                  rows="1"
+                  value={education}
+                  onChange={onChangeEducationHandler}
+                />
+                <InputGroup.Append>
+                  <Button
+                    variant="outline-secondary"
+                    id={i}
+                    onClick={(e) => deleteEducationField(i)}
+                  >
+                    Delete
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
             </Form.Group>
-          <Form.Group controlId="projects">
-            <Form.Label>Projects</Form.Label>
-            {_projects.map(projects =>
-                    <Row>
-                       <Form.Control as = "textarea" rows="1" value = {projects}/> 
-                    </Row>)}
+          ))}
+          <Button onClick={addEducationField}>Add Education</Button>
+          <Form.Group className="mt-3" controlId="experience">
+            <Form.Label>Experience</Form.Label>
           </Form.Group>
+          {_experience.map((experience, i) => (
+            <Form.Group controlId={i}>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  as="textarea"
+                  rows="1"
+                  value={experience}
+                  onChange={onChangeExperienceHandler}
+                />
+                <InputGroup.Append>
+                  <Button
+                    variant="outline-secondary"
+                    id={i}
+                    onClick={(e) => deleteExperienceField(i)}
+                  >
+                    Delete
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Form.Group>
+          ))}
+          <Button onClick={addExperienceField}>Add Experience</Button>
+          <Form.Group className="mt-3" controlId="projects">
+            <Form.Label>Projects</Form.Label>
+          </Form.Group>
+          {_projects.map((projects, i) => (
+            <Form.Group controlId={i}>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  as="textarea"
+                  rows="1"
+                  value={projects}
+                  onChange={onChangeProjectHandler}
+                />
+                <InputGroup.Append>
+                  <Button
+                    variant="outline-secondary"
+                    id={i}
+                    onClick={(e) => deleteProjectField(i)}
+                  >
+                    Delete
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Form.Group>
+          ))}
+          <Button onClick={addProjectField}>Add Project</Button>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" onClick={closeHandler}>Cancel</Button>
-        <Button variant="warning" onClick={editClickHandler}>Save changes</Button>
+        <Button variant="primary" onClick={closeHandler}>
+          Cancel
+        </Button>
+        <Button variant="warning" onClick={editClickHandler}>
+          Save changes
+        </Button>
       </Modal.Footer>
     </Modal>
-  )
+  );
 }
--- client/src/features/profile/ProfileItem.js
+++ client/src/features/profile/ProfileItem.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image'; 
-import Col from 'react-bootstrap/Col'; 
-import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
 
-import EditProfile from './EditProfile';
-import DeleteProfile from './DeleteProfile';
+import EditProfile from "./EditProfile";
+import DeleteProfile from "./DeleteProfile";
 
-export default function ProfileItem( {profile} ) {
-
+export default function ProfileItem({ profile }) {
   const [showEdit, setShowEdit] = useState(false);
   const [showDelete, setShowDelete] = useState(false);
 
@@ -19,53 +18,73 @@ export default function ProfileItem( {profile} ) {
   const handleEditClose = () => setShowEdit(false);
   const handleDeleteClose = () => setShowDelete(false);
 
-  //Fields for profile 
+  //Fields for profile
   //ADD LOGO AND RESUME FIELDS
   const { logo, education, experience, projects } = profile;
 
-  return(
+  return (
     <React.Fragment>
-      <EditProfile profile={profile} show={showEdit} closeHandler={handleEditClose}/>
-      <DeleteProfile profile={profile} show={showDelete} closeHandler={handleDeleteClose}/>
+      <EditProfile
+        profile={profile}
+        show={showEdit}
+        closeHandler={handleEditClose}
+      />
+      <DeleteProfile
+        profile={profile}
+        show={showDelete}
+        closeHandler={handleDeleteClose}
+      />
       <Container>
         <Row>
-            <Col xs={3} md={3}>
-                <Image src={logo} roundedCircle fluid/>
-            </Col>
+          <Col xs={3} md={3}>
+            <Image src={logo} roundedCircle fluid />
+          </Col>
         </Row>
       </Container>
       <Row>
-      <ListGroup className="mt-5 ml-5 mr-5">
+        <ListGroup className="mt-5 ml-5 mr-5">
           <h4>Education</h4>
-            <div className="card-education">{
-                education.map( education => <ListGroup.Item key = {education}>
-                    {education}
-                </ListGroup.Item>)}
-            </div>
+          <div className="card-education">
+            {education.map((education) => (
+              <ListGroup.Item key={education}>{education}</ListGroup.Item>
+            ))}
+          </div>
         </ListGroup>
       </Row>
       <Row>
-      <ListGroup className="mt-5 ml-5 mr-5">
+        <ListGroup className="mt-5 ml-5 mr-5">
           <h4>Experience</h4>
-            <div className="card-experience">{
-                experience.map( experience => <ListGroup.Item key = {experience}>
-                    {experience}
-                </ListGroup.Item>)}
-            </div>
-         </ListGroup>
-        </Row>
+          <div className="card-experience">
+            {experience.map((experience) => (
+              <ListGroup.Item key={experience}>{experience}</ListGroup.Item>
+            ))}
+          </div>
+        </ListGroup>
+      </Row>
       <Row>
-          <ListGroup className="mt-5 ml-5 mr-5">
-            <h4>Projects</h4>
-            <div className="card-projects">{
-                projects.map( projects => <ListGroup.Item key = {projects}>
-                    {projects}
-                </ListGroup.Item>)}            
-            </div>
-            </ListGroup>
-        </Row>
-        <Button variant="link" className="float-right" onClick={() => setShowEdit(true)}>Edit</Button>
-        <Button variant="link" className="float-right text-danger" onClick={() => setShowDelete(true)}>Delete</Button>
+        <ListGroup className="mt-5 ml-5 mr-5">
+          <h4>Projects</h4>
+          <div className="card-projects">
+            {projects.map((projects) => (
+              <ListGroup.Item key={projects}>{projects}</ListGroup.Item>
+            ))}
+          </div>
+        </ListGroup>
+      </Row>
+      <Button
+        variant="link"
+        className="float-right"
+        onClick={() => setShowEdit(true)}
+      >
+        Edit
+      </Button>
+      <Button
+        variant="link"
+        className="float-right text-danger"
+        onClick={() => setShowDelete(true)}
+      >
+        Delete
+      </Button>
     </React.Fragment>
   );
 }
