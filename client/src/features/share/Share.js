0bd53603752ec2117a6b0514bf149f3866559950
Brodie Daff <bdaff@student.unimelb.edu.au>
2020-10-01 20:31:22 +1000

    Simplify routing

--- client/src/features/share/Share.js
+++ client/src/features/share/Share.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Share() {
 
-  console.log("Share view");
+  let { username } = useParams();
+
+  console.log(`Share view for '${username}'`);
 
   return(
     <div>
