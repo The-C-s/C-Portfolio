7ae48dee8ac277c1d88366d1a29f41d828a4bc49
AlgoTron643 <nawazsaif6436@gmail.com>
2020-10-02 13:08:23 +1000

    attempted at functionality for password checks

--- client/src/features/pages/Landing.js
+++ client/src/features/pages/Landing.js
@@ -15,6 +15,58 @@ export default function Landing() {
   const loginClickHandler = () => setRegister(false);
   const registerClickHandler = () => setRegister(true);
 
+  /* tried adding the validation that was meant to handle the password checks */
+
+  validate()
+  {
+    let input = this.state.input;
+    let errors = {};
+    let isValid = true;
+
+    if (!input["name"]) {
+      isValid = false;
+      errors["name"] = "Please enter your name.";
+    }
+
+    if (!input["email"]) {
+      isValid = false;
+      errors["email"] = "Please enter your email Address.";
+    }
+
+    if (typeof input["email"] !== "undefined") {
+        
+      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+      if (!pattern.test(input["email"])) {
+        isValid = false;
+        errors["email"] = "Please enter valid email address.";
+      }
+    }
+
+    if (!input["password"]) {
+      isValid = false;
+      errors["password"] = "Please enter your password.";
+    }
+
+    if (!input["confirm_password"]) {
+      isValid = false;
+      errors["confirm_password"] = "Please enter your confirm password.";
+    }
+
+    if (typeof input["password"] !== "undefined" && typeof input["confirm_password"] !== "undefined") {
+        
+      if (input["password"] != input["confirm_password"]) {
+        isValid = false;
+        errors["password"] = "Passwords don't match.";
+      }
+    } 
+
+    this.setState({
+      errors: errors
+    });
+
+    return isValid;
+  }
+
   //const renderThis = () => register ? <Register/> : <Login onClickHandler={registerClickHandler}/>
 
   return(
