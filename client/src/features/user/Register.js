11ec872e10512a3e4af05f2c3f666b81cb26add6
AlgoTron643 <nawazsaif6436@gmail.com>
2020-10-02 11:51:39 +1000

    fixed register warning

--- client/src/features/user/Register.js
+++ client/src/features/user/Register.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-import { register, login } from './userSlice';
+import { register } from './userSlice';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
