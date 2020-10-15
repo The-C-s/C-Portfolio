8d9cdc2ce331686d4bb5bdba67654e273d1ff9e2
Brodie Daff <brodie@daff.email>
2020-10-16 00:48:38 +1100

    Write tests for user action types

--- client/src/__tests__/userSlice.test.js
+++ client/src/__tests__/userSlice.test.js
@@ -1,36 +1,24 @@
-// import configureMockStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
-// import axios from 'axios';
-
 import * as userSlice from '../features/user/userSlice';
 
-// jest.mock('axios');
-// const mockAPI = jest.mock('api');
+test('User action types are correct', () => {
 
-// const mockStore = configureMockStore([thunk]);
+  expect(userSlice.login.fulfilled.type).toEqual('user/login/fulfilled');
+  expect(userSlice.login.pending.type).toEqual('user/login/pending');
+  expect(userSlice.login.rejected.type).toEqual('user/login/rejected');
 
-describe('User functions', () => {
+  expect(userSlice.authenticate.fulfilled.type).toEqual('user/authenticate/fulfilled');
+  expect(userSlice.authenticate.pending.type).toEqual('user/authenticate/pending');
+  expect(userSlice.authenticate.rejected.type).toEqual('user/authenticate/rejected');
 
-  test('User action types are accurate', () => {
+  expect(userSlice.editUser.fulfilled.type).toEqual('user/update/fulfilled');
+  expect(userSlice.editUser.pending.type).toEqual('user/update/pending');
+  expect(userSlice.editUser.rejected.type).toEqual('user/update/rejected');
 
-    // const user = mockAPI.user;
-    // const form = { email: 'austen@maccas.edu.au', password: 'bruhmoment' }
-    // axios.get.mockResolvedValue(user);
-  
-    expect(userSlice.login.fulfilled.type).toEqual('user/login/fulfilled');
-    expect(userSlice.login.pending.type).toEqual('user/login/pending');
-    expect(userSlice.login.rejected.type).toEqual('user/login/rejected');
-  
-    expect(userSlice.authenticate.fulfilled.type).toEqual('user/authenticate/fulfilled');
-    expect(userSlice.authenticate.pending.type).toEqual('user/authenticate/pending');
-    expect(userSlice.authenticate.rejected.type).toEqual('user/authenticate/rejected');
-  
-    expect(userSlice.editUser.fulfilled.type).toEqual('user/update/fulfilled');
-    expect(userSlice.editUser.pending.type).toEqual('user/update/pending');
-    expect(userSlice.editUser.rejected.type).toEqual('user/update/rejected');
-  
-    expect(userSlice.register.fulfilled.type).toEqual('user/register/fulfilled');
-    expect(userSlice.register.pending.type).toEqual('user/register/pending');
-    expect(userSlice.register.rejected.type).toEqual('user/register/rejected');
-  });
+  expect(userSlice.register.fulfilled.type).toEqual('user/register/fulfilled');
+  expect(userSlice.register.pending.type).toEqual('user/register/pending');
+  expect(userSlice.register.rejected.type).toEqual('user/register/rejected');
+
+  expect(userSlice.editUser.fulfilled.type).toEqual('user/update/fulfilled');
+  expect(userSlice.editUser.pending.type).toEqual('user/update/pending');
+  expect(userSlice.editUser.rejected.type).toEqual('user/update/rejected');
 });
