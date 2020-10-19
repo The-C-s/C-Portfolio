11ef6856172e389c0fe8b264688fc0c9c0272dfe
Brodie Daff <brodie@daff.email>
2020-10-20 00:00:08 +1100

    Change feed and top nav

--- client/src/features/layout/TopNav.js
+++ client/src/features/layout/TopNav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import Nav from 'react-bootstrap/Nav';
@@ -7,25 +7,32 @@ import { faSearch, faBell, faCog } from '@fortawesome/free-solid-svg-icons';
 
 import NavButton from './NavButton';
 import AvatarButton from './AvatarButton';
+import EditUser from '../user/EditUser';
 
 export default function TopNav() {
+  
+  const [showUserEdit, setShowUserEdit] = useState(false);
+  const user = useSelector(state => state.user);
 
-  const avatar = useSelector(state => state.user.avatar);
+  const handleEditClose = () => setShowUserEdit(false);
 
   return(
-    <Nav className="topnav justify-content-end">
-      <Nav.Item>
-        <NavButton icon={faSearch}></NavButton>
-      </Nav.Item>
-      <Nav.Item>
-        <NavButton icon={faBell}></NavButton>
-      </Nav.Item>
-      <Nav.Item>
-        <NavButton icon={faCog}></NavButton>
-      </Nav.Item>
-      <Nav.Item>
-        <AvatarButton image={avatar}/>
-      </Nav.Item>
-    </Nav>
+    <>
+      <EditUser show={showUserEdit} closeHandler={handleEditClose} user={user}/>
+      <Nav className="topnav justify-content-end">
+        <Nav.Item className="topnav topnav-navitem topnav-search" onClick={() => console.log('\'Search\' clicked')}>
+          <NavButton icon={faSearch}></NavButton>
+        </Nav.Item>
+        <Nav.Item className="topnav topnav-navitem topnav-notifications" onClick={() => console.log('\'Notifications\' clicked')}>
+          <NavButton icon={faBell}></NavButton>
+        </Nav.Item>
+        <Nav.Item className="topnav topnav-navitem topnav-settings" onClick={() => console.log('\'Settings\' clicked')}>
+          <NavButton icon={faCog}></NavButton>
+        </Nav.Item>
+        <Nav.Item className="topnav topnav-navitem topnav-avatar" onClick={() => setShowUserEdit(true)}>
+          <AvatarButton image={user.avatar}/>
+        </Nav.Item>
+      </Nav>
+    </>
   );
 }
