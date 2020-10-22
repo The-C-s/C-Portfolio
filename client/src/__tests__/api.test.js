091aad8aa0f9c202ff7b5536e18de2eb96024a68
vyxnn <69046734+vyxnn@users.noreply.github.com>
2020-10-22 19:11:10 +1100

    more test api fixing

--- client/src/__tests__/api.test.js
+++ client/src/__tests__/api.test.js
@@ -149,11 +149,11 @@ test('Get Profile API call is correct', async () => {
 });
 
 test('Upload Logo API call is correct', async () => {
-  await api.addLogo(profile.id, profile.logo);
+  await api.addLogo(profile.logo);
   return expect(axios.post).toHaveBeenLastCalledWith(apiPaths.ADD_LOGO, profile.logo);
 });
 
 test('Upload Resume API call is correct', async () => {
-  await api.addResume(profile.id, profile.resume);
+  await api.addResume(profile.resume);
   return expect(axios.post).toHaveBeenLastCalledWith(apiPaths.ADD_RESUME, profile.resume);
 });
