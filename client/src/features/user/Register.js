d0e28d4d762381886912761b3a4cd219d242ea8d
Brodie Daff <bdaff@student.unimelb.edu.au>
2020-11-02 01:35:37 +1100

    Add form validation to Register

--- client/src/features/user/Register.js
+++ client/src/features/user/Register.js
@@ -16,31 +16,74 @@ export default function Register() {
   const dispatch = useDispatch();
   const history = useHistory();
   const registering = useSelector(state => state.app.loading.register);
+  const creatingProfile = useSelector(state => state.app.loading.createProfile);
   const loggingIn = useSelector(state => state.app.loading.login);
   const [form, updateForm] = useState({ name: '', email: '', password: '', confirmPass: ''});
-  const [formErrors, setFormErrors] = useState({});
+  const [formStatus, setFormStatus] = useState({});
   
   const onSubmitHandler = e => {
     
     e.preventDefault();
+
+    if (
+      !formStatus.isPassValid ||
+      !formStatus.doesPassMatch ||
+      !form.name ||
+      !form.email ||
+      !form.password ||
+      !form.confirmPass
+      ) {
+      console.log("Invalid form, cannot submit.");
+    }
+
     dispatch(register(form))
-    .then(dispatch(login(form)))
-    .then(dispatch(createProfile({email: form.email})))
-    .then(() => history.push('/homepage')); 
-    
+      .then(() => dispatch(login(form)))
+      .then(() => dispatch(createProfile({ email: form.email })))
+      .then(() => history.push('/homepage'));
   }
   
   const onChangeHandler = e => {
-    updateForm({ ...form, [e.target.id]: e.target.value });
-    validateForm();
+
+    updateForm({
+      ...form,
+      [e.target.id]: e.target.value
+    });
+
+    if (e.target.id === 'confirmPass') {
+      setFormStatus({
+        ...formStatus,
+        doesPassMatch: e.target.value === form.password,
+        confirmClasses: e.target.value === form.password ? 'is-valid' : 'is-invalid',
+        
+      })
+    }
   }
 
-  const validateForm = val => {
-    console.log(val);
+  const handlePassChange = passState => {
+
+    updateForm({
+      ...form,
+      password: passState.password
+    });
+
+    const comment = {
+      0: 'Too short',
+      1: 'Weak',
+      2: 'Okay',
+      3: 'Good',
+      4: 'Strong'
+    }
+
+    setFormStatus({
+      ...formStatus,
+      isPassValid: passState.isValid,
+      passClasses: passState.isValid ? 'is-valid' : 'is-invalid',
+      passComment: comment[passState.score]
+    });
   }
   
   return(
-    <div className="form-box">
+    <div className="register form-box">
       <Form className="signup-form" onSubmit={onSubmitHandler}>
         <h2>Register</h2>
         <hr/>
@@ -64,25 +107,21 @@ export default function Register() {
             className="form-control"
           />
         </Form.Group>
-        <Form.Group>
+        <Form.Group className="register-password">
           <ReactPasswordStrength
-            scoreWords={['weak', 'okay', 'good', 'strong', 'very strong']}
+            changeCallback={handlePassChange}
             inputProps={{
               id: 'password',
               placeholder: 'Password',
               value: form.password,
               onChange: onChangeHandler,
-              className: `form-control${formErrors.passStrength ? ' is-invalid' : ''}`
+              className: `form-control ${formStatus.passClasses}`
             }}
           />
-          {/* <FormInput
-            type="password"  
-            id="password" 
-            placeholder="Password" 
-            value={form.password}
-            onChange={onChangeHandler}
-            className="form-control"
-          /> */}
+          {formStatus.isPassValid !== undefined &&
+            <small className={`form-text${!formStatus.isPassValid ? ' text-danger' : ''}`}>
+              {formStatus.passComment}
+            </small>}
         </Form.Group>
         <Form.Group>
           <FormInput
@@ -91,13 +130,19 @@ export default function Register() {
             placeholder="Confirm Password" 
             value={form.confirmPass}
             onChange={onChangeHandler}
-            className="form-control"
-            invalid={true}
+            className={`form-control ${formStatus.confirmClasses}`}
           />
+          {formStatus.doesPassMatch !== undefined && !formStatus.doesPassMatch &&
+            <small className="form-text text-danger">
+              Passwords don't match
+            </small>}
         </Form.Group>
-        <Button type="submit" onClickHandler={onSubmitHandler} variant="primary">
-        {registering || loggingIn
-          ? <>Signing you up <span className="spinner-login"><HashLoader size={20} color={"#ffffff"} loading={registering}/></span></>
+        <Button type="submit" clickHandler={onSubmitHandler} variant="primary">
+        {registering || loggingIn || creatingProfile
+          ? <>
+              {creatingProfile ? 'Creating your profile' : loggingIn ? 'Logging you in' : 'Signing you up'}
+              <span className="spinner-login"><HashLoader size={20} color={"#ffffff"} loading={registering || loggingIn || creatingProfile}/></span>
+            </>
           : "Sign up"}
         </Button>
         <div className="text-center"> Already have an account?<Button as={Link} to="/" variant="link">Login here</Button></div>
