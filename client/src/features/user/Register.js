8e178f02fb6f40c2fd063214da5a1072a1aef9c2
Vikki <69046734+vyxnn@users.noreply.github.com>
2020-11-01 17:51:43 +1100

    edited format of data sent in api calls

--- client/src/features/user/Register.js
+++ client/src/features/user/Register.js
@@ -22,8 +22,8 @@ export default function Register() {
     
     e.preventDefault();
           dispatch(register(form))
-          .then(() => dispatch(login(form)))
-          .then(() => dispatch(createProfile(form.email)))
+          .then(() => {dispatch(login({email: form.email, password: form.password}))})
+          .then(() => dispatch(createProfile({email: form.email})))
           .then(() => history.push('/homepage')); 
         
   }
