a5ee531ed43f01f76200bf1f1d652ce5b850a8a9
Brodie Daff <brodie@daff.email>
2020-11-02 07:59:52 +1100

    Remove unused buttons

--- client/src/features/layout/TopNav.js
+++ client/src/features/layout/TopNav.js
@@ -3,9 +3,6 @@ import { useSelector } from 'react-redux';
 
 import Nav from 'react-bootstrap/Nav';
 
-import { faBell, faCog } from '@fortawesome/free-solid-svg-icons';
-
-import NavButton from './NavButton';
 import AvatarButton from './AvatarButton';
 import EditUser from '../user/EditUser';
 import Search from '../search/Search';
@@ -23,12 +20,6 @@ export default function TopNav() {
       <Nav className="topnav justify-content-end">
         <Nav.Item className="topnav topnav-navitem topnav-search">
           <Search/>
-        </Nav.Item>
-        <Nav.Item className="topnav topnav-navitem topnav-notifications" onClick={() => console.log('\'Notifications\' clicked')}>
-          <NavButton icon={faBell}></NavButton>
-        </Nav.Item>
-        <Nav.Item className="topnav topnav-navitem topnav-settings" onClick={() => console.log('\'Settings\' clicked')}>
-          <NavButton icon={faCog}></NavButton>
         </Nav.Item>
         <Nav.Item className="topnav topnav-navitem topnav-avatar" onClick={() => setShowUserEdit(true)}>
           <AvatarButton image={user.avatar}/>
